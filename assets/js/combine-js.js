a173ef2e2ed4e1977107c072cebdbf1a3305f3fc
harshmohite04 <mohiteharsh639@gmail.com>
2024-02-18 20:26:21 +0530

    push
    1st push

---
+++ assets/js/combine-js.js
@@ -0,0 +1,10 @@
+const logoutBtn = document.querySelector('.logout-btn');
+const addBtn = document.querySelector('.add-btn');
+const test = document.querySelector('.test');
+
+logoutBtn.addEventListener('click',()=>{
+    window.location.href="index.html";
+});
+
+
+
